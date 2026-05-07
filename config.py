4120acb0003b31ecb59f7678897ed587cabdbed1
mike <72279453+belikemikeunderscore@users.noreply.github.com>
2026-05-07 14:00:20 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+DISCORD_TOKEN = "SEU_TOKEN_AQUI"
+MEDIA_CHANNEL_ID = 123456789  # ID do canal onde o bot vai monitorar
+
+WEB_HOST = "localhost"
+WEB_PORT = 8080
+
+IMAGE_EXTENSIONS = {".jpg", ".jpeg", ".png", ".gif", ".webp"}
+VIDEO_EXTENSIONS = {".mp4", ".mov", ".webm", ".avi", ".mkv"}
